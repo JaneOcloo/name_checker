618c12b1c9d804cda85ab81e3a349f12e4ce8b50
JaneOcloo <jane.ocloo1@gmail.com>
2025-03-23 14:40:51 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-name=input("Enter your name here:👻") #takes user name as input
-print("Heyaa👋 "+ name)#greets the user
+name = input("Enter your name here:👻")  #takes user name as input
+print("Heya👋 "+ name)#greets the user
 name_length = len(name)#calculates teh number fo characters of the name
 print(f"Your name has {name_length} characters!")
 if name_length>5:
     print("You're a legend!")
 else:
     print("You're cool!")
-
